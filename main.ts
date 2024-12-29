f4212e1c2bd4523dbfbbf0aa0df7a49e3e4534b2
Dennis Muturi <deynohmuturi@gmail.com>
2024-12-29 12:32:35 +0300

    Implement user registration and login endpoints with error handling

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ import express,{Request,Response} from 'express'
 import passport from 'passport'
 import './auth/auth.ts'
 import { checkAccessToken, checkRefreshToken, createAuthTokens } from "./auth/AuthTokens.ts";
+import { UserModel } from "./model/user.ts";
+import { MongoServerError } from "mongodb";
 // import { AuthenticatedRequest } from "./types/types.ts";
 const app=express()
 async function checkAuthentication(req:any,res:any,next:any){
@@ -58,5 +60,35 @@ app.post('/protected',checkAuthentication,(req:any,res:any)=>{
 app.get('/failure',(req:any,res:any)=>{
   console.log(req.user)
   res.send('failure')
+})
+app.post('/register',async (req:any,res:any)=>{
+  console.log(req.body)
+  try {
+    const newUser = new UserModel({username: req.body.username,refreshTokenVersion:1});
+    await newUser.setPassword(req.body.password);
+    await newUser.save();
+    
+  } catch (error) {
+    console.log('register error:',error)
+    if(error instanceof MongoServerError ){
+      console.log('mongoServer error:',error.message)
+      if(error.message.includes('E11000 duplicate key error')){
+        return res.send('account already exists')
+      }
+    }
+    return res.status(500).send('failed to register')
+    
+  }
+  const { user } = await UserModel.authenticate()(req.body.username,req.body.password);
+  const authTokens=createAuthTokens(user)
+  res.json(authTokens)
+})
+app.post('/login',async(req:any,res:any)=>{
+  const { user } = await UserModel.authenticate()(req.body.username,req.body.password);
+  if(user){
+    const authTokens=createAuthTokens(user)
+    return res.json(authTokens)
+  }
+  res.send('wrong password or username')
 })
 app.listen(3000,()=>console.log('running'))
