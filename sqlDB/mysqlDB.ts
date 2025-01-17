abeb603cc7fc8e85f2a88e000ee3b1a8cc8e7add
Dennis Muturi <deynohmuturi@gmail.com>
2025-01-17 15:44:40 +0300

    Refactor MySQL user registration functions and remove unused username field from oAuthUser type

--- sqlDB/mysqlDB.ts
+++ sqlDB/mysqlDB.ts
@@ -8,7 +8,6 @@ const access: PoolOptions = {
   database: 'foodium',
 };
 type oAuthUser={
-    username:string,
     email:string,
     google_id:string
 }
@@ -16,48 +15,49 @@ type passwordUser={
     username:string,
     email:string,
     password:string,
-    salt:string,
 }
 
 const connection = await mysql.createPool(access);
 // const results = await connection.query("CALL add_oauth_user('googleusertester8','good8@gmail.com','googly')");
 // console.log(results[0]);
 export async function registerOauthUser(registerRequest:oAuthUser){
-    const results = await connection.query(`CALL add_oauth_user(?,?,?)`,[registerRequest.username,registerRequest.email,registerRequest.google_id]);
+    const results = await connection.query(`CALL add_oauth_user(?,?)`,[registerRequest.email,registerRequest.google_id]);
     return results[0]
 }
 export async function registerPasswordUser(registerRequest:passwordUser){
-    const results = await connection.query(`CALL add_password_user(?,?,?,?)`,[registerRequest.username,registerRequest.email,registerRequest.password,registerRequest.salt]);
+    const results = await connection.query(`CALL add_password_user(?,?,?)`,[registerRequest.username,registerRequest.email,registerRequest.password]);
     return results[0]
 }
-export async function checkIfOAuthUserExists(google_id:string){
-    const results=await connection.query(`CALL retrieve_google_oauth_user(?)`,[google_id])
-    return results[0]
-}
-export async function checkIfPasswordUserExists(email:string){
-    const results=await connection.query(`CALL retrieve_password_user(?)`,[email])
+export async function checkIfPasswordUserExists(username:string){
+    const results=await connection.query(`CALL retrieve_password_user(?)`,[username])
     return results[0]
 }
 export async function retrieveUser(id:number){
     const results=await connection.query(`CALL retrieve_user(?)`,[id])
     return results[0]
 }
-
-
-
-const testuser:oAuthUser={
-    username:'dennis',
-    email:'mutz@mutz.com',
-    google_id:'googler',
+export async function checkUsernameAvaliabilty(username:string){
+    const results=await connection.query(`CALL check_username_availability(?)`,[username])
+    return results[0][0][0]
 }
-const testuser2:passwordUser={
-    username:'dennis',
-    email:'mutz@mutz.com',
-    password:'googler',
-    salt:'saltChumvi'
-}
+// const test=await checkUsernameAvaliabilty('mimi2')
+// console.log(test)
+
+
+
+// const testuser:oAuthUser={
+//     email:'mutz@mutz.com',
+//     google_id:'testgoogle5',
+// }
+// const testuser2:passwordUser={
+//     username:'dennis',
+//     email:'mutz@mutz.com',
+//     password:'googler',
+//     salt:'saltChumvi'
+// }
 // const result1=await registerOauthUser(testuser)
-// console.log('result 1:',result1)
+// console.log('result 1:',result1[0][0])
+
 // const result2=await registerPasswordUser(testuser2)
 // console.log('result 2:',result2)
 // const result3=await checkIfOAuthUserExists('googly')
