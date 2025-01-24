dba299de3e76bbe7d8b7e9986f33eb0b0938515a
Dennis Muturi <deynohmuturi@gmail.com>
2025-01-24 16:21:27 +0300

    No code changes made.

--- RouteHandlers/routesHandler.ts
+++ RouteHandlers/routesHandler.ts
@@ -2,7 +2,7 @@
 import {RequestHandler} from 'express'
 import { checkAccessToken,checkRefreshToken, createAuthTokens } from "../auth/AuthTokens.ts";
 import * as bcrypt from "https://deno.land/x/bcrypt/mod.ts";
-import { addRating, addRecipeIntake, addReview, checkIfPasswordUserExists, checkUsernameAvailability, findRecipeReviews, getPaginatedRecipes, Rating, registerPasswordUser, Review, updateOauthUserUsername } from "../sqlDB/mysqlDB.ts";
+import { addFoodIntake, addRating, addRecipeIntake, addReview, checkIfPasswordUserExists, checkUsernameAvailability, findRecipeReviews, findUserFoodIntake, getPaginatedRecipes, Rating, registerPasswordUser, Review, updateOauthUserUsername } from "../sqlDB/mysqlDB.ts";
 import { findUserRatings } from "../sqlDB/mysqlDB.ts";
 import { findUserReviews } from "../sqlDB/mysqlDB.ts";
 import {findUserRecipeIntake} from '../sqlDB/mysqlDB.ts'
@@ -22,50 +22,65 @@ interface LoginRequestBody{
 }
 interface GetPaginatedRecipesBody{
     numberOfResults:number
-    next?:number
+    next?:string
+    region:string
 }
 interface OauthAddUsernameBody{
     username:string
 }
 interface ReviewBody{
     reviewText:string
-    recipeId:number;
+    recipeId:string;
+    region:string
 
 }
 interface RatingBody{
     ratingNumber:number;
-    recipeId:number;
+    recipeId:string;
+    region:string
 }
 interface getReviewsBody{
-    recipeId:number;
+    recipeId:string;
     numberOfResults:number;
-    next?:number
+    next?:string
+    region:string
 }
 interface addRecipeIntakeBody{
-    recipeId:number;
+    recipeId:string;
+    region:string
+}
+interface addFoodIntakeBody{
+    recipeId:string;
 }
 interface getUserReviewsBody{
-    next?:number
+    next?:string
+    region:string
 }
 interface getUserRatingsBody{
-    next?:number
+    next?:string
+    region:string
+
+}
+interface getUserFoodIntakeBody{
+    next?:string
+
 }
 export const getUserRecipeIntakeHandler:RequestHandler=async (req,res)=>{
     const getUserRecipeIntakeInfo=<getUserRatingsBody>req.body
     try {
         if(getUserRecipeIntakeInfo.next){
-            const results=await findUserRecipeIntake(Number(req.userId),getUserRecipeIntakeInfo.next)
+            const results=await findUserRecipeIntake(req.userId,getUserRecipeIntakeInfo.region,getUserRecipeIntakeInfo.next)
             if(results.length>0){
-                return res.json({results,next:results[results.length-1]['id'],newTokens:req.newTokens})
+                return res.json({results,next:results[results.length-1]['uuid'],newTokens:req.newTokens})
             }
             else{
                 return res.send('no user recipe intake available')
             }
         }
         else{
-            const results=await findUserRecipeIntake(Number(req.userId))
+            const results=await findUserRecipeIntake(req.userId,getUserRecipeIntakeInfo.region)
             if(results.length>0){
-                return res.json({results,next:results[results.length-1]['id'],newTokens:req.newTokens})
+                return res.json({results,next:results[results.length-1]['uuid'],newTokens:req.newTokens})
             }
             else{
                 return res.send('no user recipe intake available')
@@ -73,26 +88,53 @@ export const getUserRecipeIntakeHandler:RequestHandler=async (req,res)=>{
         }
         
     } catch (error) {
-        console.log('error at get user ratings',error)
-        return res.status(404).send('an error occurred while retrieving user ratings,try again')    
+        console.log('error at get user recipe intake',error)
+        return res.status(404).send('an error occurred while retrieving user recipe intake,try again')    
+    }
+}
+export const getUserFoodIntakeHandler:RequestHandler=async (req,res)=>{
+    const getUserRecipeIntakeInfo=<getUserFoodIntakeBody>req.body
+    try {
+        if(getUserRecipeIntakeInfo.next){
+            const results=await findUserFoodIntake(req.userId,getUserRecipeIntakeInfo.next)
+            if(results.length>0){
+                return res.json({results,next:results[results.length-1]['uuid'],newTokens:req.newTokens})
+            }
+            else{
+                return res.send('no user food intake available')
+            }
+        }
+        else{
+            const results=await findUserFoodIntake(req.userId)
+            if(results.length>0){
+                return res.json({results,next:results[results.length-1]['uuid'],newTokens:req.newTokens})
+            }
+            else{
+                return res.send('no user food intake available')
+            }
+        }
+        
+    } catch (error) {
+        console.log('error at get user food intake',error)
+        return res.status(404).send('an error occurred while retrieving user food intake,try again')    
     }
 }
 export const getUserRatingsHandler:RequestHandler=async (req,res)=>{
     const getUserRatingsInfo=<getUserRatingsBody>req.body
     try {
         if(getUserRatingsInfo.next){
-            const results=await findUserRatings(Number(req.userId),getUserRatingsInfo.next)
+            const results=await findUserRatings(req.userId,getUserRatingsInfo.region,getUserRatingsInfo.next)
             if(results.length>0){
-                return res.json({results,next:results[results.length-1]['id'],newTokens:req.newTokens})
+                return res.json({results,next:results[results.length-1]['uuid'],newTokens:req.newTokens})
             }
             else{
                 return res.send('no user ratings available')
             }
         }
         else{
-            const results=await findUserRatings(Number(req.userId))
+            const results=await findUserRatings(req.userId,getUserRatingsInfo.region)
             if(results.length>0){
-                return res.json({results,next:results[results.length-1]['id'],newTokens:req.newTokens})
+                return res.json({results,next:results[results.length-1]['uuid'],newTokens:req.newTokens})
             }
             else{
                 return res.send('no user ratings available')
@@ -105,21 +147,21 @@ export const getUserRatingsHandler:RequestHandler=async (req,res)=>{
     }
 }
 export const getUserReviewsHandler:RequestHandler=async (req,res)=>{
-    const getUserReviewsInfo=<getUserRatingsBody>req.body
+    const getUserReviewsInfo=<getUserReviewsBody>req.body
     try {
         if(getUserReviewsInfo.next){
-            const results=await findUserReviews(Number(req.userId),getUserReviewsInfo.next)
+            const results=await findUserReviews(req.userId,getUserReviewsInfo.region,getUserReviewsInfo.next)
             if(results.length>0){
-                return res.json({results,next:results[results.length-1]['id'],newTokens:req.newTokens})
+                return res.json({results,next:results[results.length-1]['uuid'],newTokens:req.newTokens})
             }
             else{
                 return res.send('no user reviews available')
             }
         }
         else{
-            const results=await findUserReviews(Number(req.userId))
+            const results=await findUserReviews(req.userId,getUserReviewsInfo.region)
             if(results.length>0){
-                return res.json({results,next:results[results.length-1]['id'],newTokens:req.newTokens})
+                return res.json({results,next:results[results.length-1]['uuid'],newTokens:req.newTokens})
             }
             else{
                 return res.send('no user reviews available')
@@ -137,7 +179,20 @@ export const addRecipeIntakeHandler:RequestHandler=async (req,res)=>{
         return res.status(404).send('provide recipe id')
     }
     try {
-        const results=await addRecipeIntake({userId:Number(req.userId),recipeId:addRecipeIntakeInfo.recipeId})
+        const results=await addRecipeIntake({userId:req.userId,region:addRecipeIntakeInfo.region,recipeId:addRecipeIntakeInfo.recipeId})
+        return res.json({...results,newTokens:req.newTokens})
+    } catch (error) {
+        console.log('error at add recipe intake',error)
+        return res.status(404).send('an error occurred while adding recipe intake,try again')     
+    }
+}
+export const addFoodIntakeHandler:RequestHandler=async (req,res)=>{
+    const addRecipeIntakeInfo=<addFoodIntakeBody>req.body
+    if(!addRecipeIntakeInfo.recipeId){
+        return res.status(404).send('provide recipe id')
+    }
+    try {
+        const results=await addFoodIntake({userId:req.userId,foodId:addRecipeIntakeInfo.recipeId})
         return res.json({...results,newTokens:req.newTokens})
     } catch (error) {
         console.log('error at add recipe intake',error)
@@ -151,37 +206,37 @@ export const getReviewsHandler:RequestHandler=async (req,res)=>{
     }
     try {
         if(getReviewsInfo.numberOfResults&&getReviewsInfo.next){
-            const reviews=await findRecipeReviews(getReviewsInfo.recipeId,getReviewsInfo.numberOfResults,getReviewsInfo.next)
+            const reviews=await findRecipeReviews(getReviewsInfo.recipeId,getReviewsInfo.region,getReviewsInfo.numberOfResults,getReviewsInfo.next)
             console.log(reviews)
             if(reviews.length>0){
-                return res.json({results:reviews,next:reviews[reviews.length-1]['id'],newTokens:req.newTokens})
+                return res.json({results:reviews,next:reviews[reviews.length-1]['uuid'],newTokens:req.newTokens})
             }
             else{
                 return res.send('no reviews available')
             }
         }
         else if(getReviewsInfo.numberOfResults&&!getReviewsInfo.next){
-            const reviews=await findRecipeReviews(getReviewsInfo.recipeId,getReviewsInfo.numberOfResults)
+            const reviews=await findRecipeReviews(getReviewsInfo.recipeId,getReviewsInfo.region,getReviewsInfo.numberOfResults)
             if(reviews.length>0){
-                return res.json({results:reviews,next:reviews[reviews.length-1]['id'],newTokens:req.newTokens})
+                return res.json({results:reviews,next:reviews[reviews.length-1]['uuid'],newTokens:req.newTokens})
             }
             else{
                 return res.send('no reviews available')
             }
         }
         else if(!getReviewsInfo.numberOfResults&&getReviewsInfo.next){
-            const reviews=await findRecipeReviews(getReviewsInfo.recipeId,5,getReviewsInfo.next)
+            const reviews=await findRecipeReviews(getReviewsInfo.recipeId,getReviewsInfo.region,5,getReviewsInfo.next)
             if(reviews.length>0){
-                return res.json({results:reviews,next:reviews[reviews.length-1]['id'],newTokens:req.newTokens})
+                return res.json({results:reviews,next:reviews[reviews.length-1]['uuid'],newTokens:req.newTokens})
             }
             else{
                 return res.send('no reviews available')
             }
         }
         else{
-            const reviews=await findRecipeReviews(getReviewsInfo.recipeId,5)
+            const reviews=await findRecipeReviews(getReviewsInfo.recipeId,getReviewsInfo.region,5)
             if(reviews.length>0){
-                return res.json({results:reviews,next:reviews[reviews.length-1]['id'],newTokens:req.newTokens})
+                return res.json({results:reviews,next:reviews[reviews.length-1]['uuid'],newTokens:req.newTokens})
             }
             else{
                 return res.send('no reviews available')
@@ -203,7 +258,8 @@ export const addRecipeReviewHandler:RequestHandler=async (req,res)=>{
         const review:Review={
             reviewText:addReviewInfo.reviewText,
             recipeId:addReviewInfo.recipeId,
-            reviewerId:Number(req.userId)
+            reviewerId:req.userId,
+            region:addReviewInfo.region
         }
         const addReviewResult=await addReview(review)
         if(addReviewResult.task=='not added'){
@@ -225,7 +281,8 @@ export const addRecipeRatingHandler:RequestHandler=async (req,res)=>{
         const rating:Rating={
             ratingNumber:addRatingInfo.ratingNumber,
             recipeId:addRatingInfo.recipeId,
-            raterId:Number(req.userId)
+            raterId:req.userId,
+            region:addRatingInfo.region
         }
         const addRatingResult=await addRating(rating)
         if(addRatingResult.task=='not added'){
@@ -250,7 +307,7 @@ export const addUsernameForOauthHandler:RequestHandler=async(req,res)=>{
             return res.status(404).send(`the username ${oauthAddusernameInfo.username} is already taken,choose another one`)
         }
         else{
-            const user=await updateOauthUserUsername(oauthAddusernameInfo.username,Number(req.userId))
+            const user=await updateOauthUserUsername(oauthAddusernameInfo.username,req.userId)
             return res.json({...user,newTokens:req.newTokens})
         }
         
@@ -264,37 +321,37 @@ export const fetchPaginatedRecipesHandler:RequestHandler=async(req,res)=>{
     const pageInfo=<GetPaginatedRecipesBody>req.body
     try {
         if(pageInfo.numberOfResults&&pageInfo.next){
-            const recipes=await getPaginatedRecipes(pageInfo.numberOfResults,pageInfo.next)
+            const recipes=await getPaginatedRecipes(pageInfo.numberOfResults,pageInfo.region,pageInfo.next)
             const recipesResponse={
                 results:recipes,
-                next:recipes[recipes.length-1]['id'],
+                next:recipes[recipes.length-1]['uuid'],
                 newTokens:req.newTokens
             }
             return res.json(recipesResponse)
         }
         else if(!pageInfo.numberOfResults&&pageInfo.next){
-            const recipes=await getPaginatedRecipes(5,pageInfo.next)
+            const recipes=await getPaginatedRecipes(5,pageInfo.region,pageInfo.next)
             const recipesResponse={
                 results:recipes,
-                next:recipes[recipes.length-1]['id'],
+                next:recipes[recipes.length-1]['uuid'],
                 newTokens:req.newTokens
             }
             return res.json(recipesResponse)
         }
         else if(pageInfo.numberOfResults&&!pageInfo.next){
-            const recipes=await getPaginatedRecipes(pageInfo.numberOfResults)
+            const recipes=await getPaginatedRecipes(pageInfo.numberOfResults,pageInfo.region)
             const recipesResponse={
                 results:recipes,
-                next:recipes[recipes.length-1]['id'],
+                next:recipes[recipes.length-1]['uuid'],
                 newTokens:req.newTokens
             }
             return res.json(recipesResponse)
         }
         else{
-            const recipes=await getPaginatedRecipes(5)
+            const recipes=await getPaginatedRecipes(5,pageInfo.region)
             const recipesResponse={
                 results:recipes,
-                next:recipes[recipes.length-1]['id'],
+                next:recipes[recipes.length-1]['uuid'],
                 newTokens:req.newTokens
             }
             return res.json(recipesResponse)
@@ -307,6 +364,7 @@ export const fetchPaginatedRecipesHandler:RequestHandler=async(req,res)=>{
         
     }
 }
+
 export const loginRouteHandler:RequestHandler=async (req,res)=>{
     try {
         const loginInfo=<LoginRequestBody>req.body
@@ -323,7 +381,7 @@ export const loginRouteHandler:RequestHandler=async (req,res)=>{
             return res.status(404).send('wrong password')
         }
         const userInfo={
-            id:possibleUser[0]['id'],
+            id:possibleUser[0]['userId'],
             refreshTokenVersion:possibleUser[0]['refreshTokenversion'],
         }
         const userTokens=createAuthTokens(userInfo)
@@ -354,7 +412,7 @@ export const registerRouteHandler:RequestHandler=async (req,res)=>{
         }
         const user=await registerPasswordUser(userObj)
         const registeredUser={
-            id:user['last_insert_id()'],
+            id:user['userId'],
             refreshTokenVersion:0
         }
         const userTokens=createAuthTokens(registeredUser)
